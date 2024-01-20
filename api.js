9c070e2699f43579d7a9b9814a95df21ae40fe31
souravbhowmik1999 <sourav.bhowmik@tekditechnologies.com>
2024-01-20 17:16:00 +0530

    Text to audio convertion

--- api.js
+++ api.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 require('dotenv').config(); // Load environment variables from .env file
 
 const apiUrl = 'https://demo-api.models.ai4bharat.org/inference/asr/whisper';
+const sttUrl = 'https://demo-api.models.ai4bharat.org/inference/tts';
 
 // Function to send an audio file to the AI4Bharat API and transcribe it
 async function transcribeAudio(audioFilePath) {
@@ -39,4 +40,37 @@ async function transcribeAudio(audioFilePath) {
   }
 }
 
-module.exports = { transcribeAudio };
+
+async function textToAudio(text) {
+  try {
+    const requestBody = {
+      controlConfig: {
+        dataTracking: true
+      },
+      input: [
+        {
+          source: text
+        }
+      ],
+      config: {
+        gender: "female",
+        language: {
+          sourceLanguage: "en"
+        }
+      }
+    };
+
+    const response = await axios.post(sttUrl, requestBody, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    return response;
+  } catch (error) {
+    console.error('Error from AI4Bharat api:', error);
+    throw error;
+  }
+}
+
+module.exports = { transcribeAudio, textToAudio };
